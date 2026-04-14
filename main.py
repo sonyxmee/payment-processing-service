26269eb93458fdaa77ef2184d1222cf12767979c
sbogdanova <sbogdanova@tcinet.ru>
2026-04-14 16:53:23 +0300

    init project

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    print("Hello from payment!")
+
+
+if __name__ == "__main__":
+    main()
+
+# uv init <project name>
+# uv venv --python /usr/bin/python3.14
+# uv python pin /usr/bin/python3.14
